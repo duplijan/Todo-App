f32d8ccd610f0aba76051f144a10fb1daef58c14
Jan Duplinszki <duplinszkijan@gmail.com>
2018-10-30 13:27:55 +0000

    Update NewTodo.js

--- src/Components/NewTodo.js
+++ src/Components/NewTodo.js
@@ -5,7 +5,7 @@ const NewTodo = (props) => {
     <div className='newTodo'>
       <form onSubmit={props.handleSubmit}>
         <label htmlFor='new-todo'>
-          What's needed to be done?
+          What is needed to be done?
         </label><br />
         <input id='new-todo' onChange={props.handleChange} value={props.newTodo} type='text' placeholder='new todo...' />
         <button type='submit' className='addTodo_button'>
